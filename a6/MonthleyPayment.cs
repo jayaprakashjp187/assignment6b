68ad12a124a6688328f2d3a39a823dc35e7a9983
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-30 20:07:28 +0530

    monthley payment

---
+++ a6/MonthleyPayment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a6
+{
+    internal class MonthleyPayment
+    {
+        public static double CalculateMonthlyPayment(double a, double b, double c)
+        {
+
+            double n = 12 * b;
+
+
+            double r = c / (12 * 100);
+
+
+            double payment = (a * r) / (1 - Math.Pow(1 + r, -n));
+
+            return payment;
+        }
+    }
+}
---
+++ a6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace a6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose what program u want");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    Console.WriteLine("1.MonthleyPaymentProgram");
+                    Console.WriteLine("Enter theyears ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter the loan amount: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter the percent interest ");
+                    int c = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("The Monthly Payment  is:" + MonthleyPayment.CalculateMonthlyPayment(a, b, c));
+
+
+                    break;
+            }
+        }
+    }
+}
